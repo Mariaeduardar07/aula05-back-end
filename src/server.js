9b67b1c957c00ec10a46e97957e725762aa4876b
maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2024-08-29 10:49:40 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,12 +8,59 @@ const serverPort = process.env.PORT || 4000
 const app = express() 
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+    {
+        id: 2,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+    {
+        id: 3,
+        nome: "Ansiedade",
+        cor: "Laranja"
+    }
+]
+
+const personagens = [
+    {
+        id: 100,
+        nome: "Totoro",
+        studio: "Ghibli",
+        vivo: true
+    },
+    {
+        id: 101,
+        nome: "Rapunzel",
+        studio: "Disney",
+        vivo: true
+    },
+    {
+        id: 102,
+        nome: "Jessie",
+        studio: "Pixar",
+        vivo: true
+    }
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, Word!" })
 })
 
 app.get("/2tds2", (req, res) => {
     return res.status(200).send({ message: "Hello, Word!" })
+})
+
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send(emocoes)
+})
+
+app.get("/personagens", (req, res) => {
+    return res.status(200).send(personagens)
 })
 
 app.listen(serverPort, () => {
