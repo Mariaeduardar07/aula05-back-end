46afaac8091c3bdb88dd77b0cdc2729ae02633f8
maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2024-08-29 11:23:04 -0300

    feat: add create emotion request

--- src/server.js
+++ src/server.js
@@ -63,6 +63,18 @@ app.get("/personagens", (req, res) => {
     return res.status(200).send(personagens)
 })
 
+app.post("/emocoes", (req, res) => {
+    const { nome, cor } = req.body
+    const novaEmocao = {
+        id: emocoes.length + 1,
+        nome: nome,
+        cor: cor
+    }
+    emocoes.push(novaEmocao)
+    return res.status(200).send(emocoes)
+})
+
+
 app.listen(serverPort, () => {
 console.log(`✨ Server started on http://localhost:${serverPort}`)
 })
