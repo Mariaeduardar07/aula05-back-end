9dfc98537f6827627770657b7fd8dc8f62d7566b
maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2024-09-26 17:06:55 -0300

    feat: start to create characters routes

--- src/routes/personagens.routes.js
+++ src/routes/personagens.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 
-const emocoesRoutes = Router();
+const personagensRoutes = Router();
 
-let personagensRoutes = [
+let personagens = [
     {
         id: 100,
         nome: "Totoro",
@@ -23,6 +23,36 @@ let personagensRoutes = [
     }
 ]
 
-app.get("/personagens", (req, res) => {
+personagensRoutes.get("/", (req, res) => {
     return res.status(200).send(personagens)
 });
+//Rota para criar uma nova emoção
+personagensRoutes.post("/", (req, res) => {
+    const { nome, studio, vivo } = req.body
+    const novoPersonagem = {
+        id: personagens.length + 1,
+        nome: nome, 
+        studio: studio,
+        vivo: vivo
+    }
+    personagens.push(novoPersonagem)
+    return res.status(201).send(novoPersonagem)
+});
+
+personagensRoutes.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    
+    const personagem = personagens.find((person) => person.id == id);
+
+        if(!person) {
+            return res.status(404).send({message: "Personagem Não Encontado"});
+        }
+
+        personagens = personagens.filter((person) => person.id != id);
+        
+        return res.status(200).send({
+            message: "Personagem Deletado", personagem
+           });
+})
+
+export default personagensRoutes
